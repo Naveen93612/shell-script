773b34516ed15767e6984f68cae72dc1c0b54a40
naveen93612 <naveen93612@gmail.com>
2025-12-13 12:18:12 +0530

    checking passwords

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MOVIES=("PUSHPA" "RRR" "DEVARA")
+
+echo "first movie name: ${MOVIES[1]}"
+echo "Second movie name: ${MOVIES[2]}"
+echo "Third movie name: ${MOVIES[3]}"
+
+echo "All movies name: ${MOVIES[@]}"
