ed18c2c5750b08ad324cb3dd32ee31a0fb4cf63d
naveen93612 <naveen93612@gmail.com>
2025-12-15 15:21:05 +0530

    functions

--- 13-logs.sh
+++ 13-logs.sh
@@ -6,7 +6,7 @@ Y="\e[33m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/LOG_FILE-TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -18,13 +18,14 @@ else
 fi
 
 }
+echo "script started excuting at :: $TIMESTAMP" &>>$LOG_FILE
 
 if [ $USERID -ne 0 ]
 then 
 echo "ERROR:: you must have sudo access to excute this"
 fi
 
-dnf list installed mysql
+dnf list installed mysql &>>LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
     dnf install mysql -y &>>LOG_FILE_NAME
@@ -34,7 +35,7 @@ else
 fi
 
 
-dnf list installed git
+dnf list installed git &>>LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
     dnf install git -y &>>LOG_FILE_NAME
