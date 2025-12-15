c3e23b503638f9978163328e9b7f17612fa46e7e
naveen93612 <naveen93612@gmail.com>
2025-12-15 14:34:10 +0530

    functions

---
+++ 12-colors.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then
+    echo -e "$2...$R FAILURE $N"
+    exit 1
+else
+    echo -e "$2...$G SUCCESS $N"
+fi
+
+}
+
+if [ $USERID -ne 0 ]
+then 
+echo "ERROR:: you must have sudo access to excute this"
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "installing mysql"
+else 
+    echo -e "mysql already $Y installed $N"
+fi
+
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "installing git"
+else 
+    echo -e "git already $Yinstalled $N"
+fi
