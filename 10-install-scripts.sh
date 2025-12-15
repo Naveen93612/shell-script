cb1e7479b3b15bf7c50c1f0b3f36a385ccb66ef7
naveen93612 <naveen93612@gmail.com>
2025-12-15 12:30:45 +0530

    exit status

--- 10-install-scripts.sh
+++ 10-install-scripts.sh
@@ -6,4 +6,19 @@ echo "ERROR:: you must have sudo access to excute this"
 fi
 
 dnf install mysql -y
+if [ $? -ne 0 ]
+then
+echo "installing mysql...FAILURE"
+exit 1
+else
+echo "installing mysql....SUCCESS"
+fi
+
 dnf install git -y
+if [ $? -ne 0 ]
+then 
+echo "installing git...FAILURE"
+exit 1
+else 
+echo "installing git...SUCCESS"
+fi
